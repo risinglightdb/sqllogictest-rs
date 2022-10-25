5fb0751d6e1e24e9ce1154e5e666d00f9208b68b
ZENOTME <43447882+ZENOTME@users.noreply.github.com>
2022-10-26 00:26:39 +0800

    support more type in postgres-extended (#94)
    * * support INTERVAL && TIMESTAMPTZ type
    
    Signed-off-by: Zejiong Dong <810918843@qq.com>
    
    * * add test
    * support array type
    
    Signed-off-by: Zejiong Dong <810918843@qq.com>
    
    Signed-off-by: Zejiong Dong <810918843@qq.com>

--- sqllogictest-bin/src/engines/postgres_extended.rs
+++ sqllogictest-bin/src/engines/postgres_extended.rs
@@ -2,7 +2,8 @@ use std::sync::Arc;
 
 use anyhow::Context;
 use async_trait::async_trait;
-use chrono::{NaiveDate, NaiveDateTime, NaiveTime};
+use chrono::{DateTime, NaiveDate, NaiveDateTime, NaiveTime};
+use pg_interval::Interval;
 use postgres_types::Type;
 use rust_decimal::Decimal;
 use tokio::task::JoinHandle;
@@ -73,6 +74,60 @@ macro_rules! array_process {
             }
         }
     };
+    ($row:ident, $output:ident, $idx:ident, $t:ty, $convert:ident) => {
+        let value: Option<Vec<Option<$t>>> = $row.get($idx);
+        match value {
+            Some(value) => {
+                write!($output, "{{").unwrap();
+                for (i, v) in value.iter().enumerate() {
+                    match v {
+                        Some(v) => {
+                            write!($output, "{}", $convert(v)).unwrap();
+                        }
+                        None => {
+                            write!($output, "NULL").unwrap();
+                        }
+                    }
+                    if i < value.len() - 1 {
+                        write!($output, ",").unwrap();
+                    }
+                }
+                write!($output, "}}").unwrap();
+            }
+            None => {
+                write!($output, "NULL").unwrap();
+            }
+        }
+    };
+    ($self:ident, $row:ident, $output:ident, $idx:ident, $t:ty, $ty_name:expr) => {
+        let value: Option<Vec<Option<$t>>> = $row.get($idx);
+        match value {
+            Some(value) => {
+                write!($output, "{{").unwrap();
+                for (i, v) in value.iter().enumerate() {
+                    match v {
+                        Some(v) => {
+                            let sql = format!("select ($1::{})::varchar", stringify!($ty_name));
+                            let tmp_rows = $self.client.query(&sql, &[&v]).await.unwrap();
+                            let value: &str = tmp_rows.get(0).unwrap().get(0);
+                            assert!(value.len() > 0);
+                            write!($output, "{}", value).unwrap();
+                        }
+                        None => {
+                            write!($output, "NULL").unwrap();
+                        }
+                    }
+                    if i < value.len() - 1 {
+                        write!($output, ",").unwrap();
+                    }
+                }
+                write!($output, "}}").unwrap();
+            }
+            None => {
+                write!($output, "NULL").unwrap();
+            }
+        }
+    };
 }
 
 macro_rules! single_process {
@@ -87,6 +142,72 @@ macro_rules! single_process {
             }
         }
     };
+    ($row:ident, $output:ident, $idx:ident, $t:ty, $convert:ident) => {
+        let value: Option<$t> = $row.get($idx);
+        match value {
+            Some(value) => {
+                write!($output, "{}", $convert(&value)).unwrap();
+            }
+            None => {
+                write!($output, "NULL").unwrap();
+            }
+        }
+    };
+    ($self:ident, $row:ident, $output:ident, $idx:ident, $t:ty, $ty_name:expr) => {
+        let value: Option<$t> = $row.get($idx);
+        match value {
+            Some(value) => {
+                let sql = format!("select ($1::{})::varchar", stringify!($ty_name));
+                let tmp_rows = $self.client.query(&sql, &[&value]).await.unwrap();
+                let value: &str = tmp_rows.get(0).unwrap().get(0);
+                assert!(value.len() > 0);
+                write!($output, "{}", value).unwrap();
+            }
+            None => {
+                write!($output, "NULL").unwrap();
+            }
+        }
+    };
+}
+
+fn bool_to_str(value: &bool) -> &'static str {
+    if *value {
+        "t"
+    } else {
+        "f"
+    }
+}
+
+fn varchar_to_str(value: &str) -> String {
+    if value.is_empty() {
+        "(empty)".to_string()
+    } else {
+        value.to_string()
+    }
+}
+
+fn float4_to_str(value: &f32) -> String {
+    if value.is_nan() {
+        "NaN".to_string()
+    } else if *value == f32::INFINITY {
+        "Infinity".to_string()
+    } else if *value == f32::NEG_INFINITY {
+        "-Infinity".to_string()
+    } else {
+        value.to_string()
+    }
+}
+
+fn float8_to_str(value: &f64) -> String {
+    if value.is_nan() {
+        "NaN".to_string()
+    } else if *value == f64::INFINITY {
+        "Infinity".to_string()
+    } else if *value == f64::NEG_INFINITY {
+        "-Infinity".to_string()
+    } else {
+        value.to_string()
+    }
 }
 
 #[async_trait]
@@ -113,23 +234,7 @@ impl sqllogictest::AsyncDB for PostgresExtended {
                     if idx != 0 {
                         write!(output, " ").unwrap();
                     }
-
                     match column.type_().clone() {
-                        Type::VARCHAR | Type::TEXT => {
-                            let value: Option<&str> = row.get(idx);
-                            match value {
-                                Some(value) => {
-                                    if value.is_empty() {
-                                        write!(output, "(empty)").unwrap();
-                                    } else {
-                                        write!(output, "{}", value).unwrap();
-                                    }
-                                }
-                                None => {
-                                    write!(output, "NULL").unwrap();
-                                }
-                            }
-                        }
                         Type::INT2 => {
                             single_process!(row, output, idx, i16);
                         }
@@ -139,66 +244,20 @@ impl sqllogictest::AsyncDB for PostgresExtended {
                         Type::INT8 => {
                             single_process!(row, output, idx, i64);
                         }
-                        Type::BOOL => {
-                            let value: Option<bool> = row.get(idx);
-                            match value {
-                                Some(value) => {
-                                    if value {
-                                        write!(output, "t").unwrap();
-                                    } else {
-                                        write!(output, "f").unwrap();
-                                    }
-                                }
-                                None => {
-                                    write!(output, "NULL").unwrap();
-                                }
-                            }
-                        }
-                        Type::FLOAT4 => {
-                            let value: Option<f32> = row.get(idx);
-                            match value {
-                                Some(value) => {
-                                    if value == f32::INFINITY {
-                                        write!(output, "Infinity").unwrap();
-                                    } else if value == f32::NEG_INFINITY {
-                                        write!(output, "-Infinity").unwrap();
-                                    } else {
-                                        write!(output, "{}", value).unwrap();
-                                    }
-                                }
-                                None => {
-                                    write!(output, "NULL").unwrap();
-                                }
-                            }
-                        }
-                        Type::FLOAT8 => {
-                            let value: Option<f64> = row.get(idx);
-                            match value {
-                                Some(value) => {
-                                    if value == f64::INFINITY {
-                                        write!(output, "Infinity").unwrap();
-                                    } else if value == f64::NEG_INFINITY {
-                                        write!(output, "-Infinity").unwrap();
-                                    } else {
-                                        write!(output, "{}", value).unwrap();
-                                    }
-                                }
-                                None => {
-                                    write!(output, "NULL").unwrap();
-                                }
-                            }
-                        }
                         Type::NUMERIC => {
                             single_process!(row, output, idx, Decimal);
-                        }
-                        Type::TIMESTAMP => {
-                            single_process!(row, output, idx, NaiveDateTime);
                         }
                         Type::DATE => {
                             single_process!(row, output, idx, NaiveDate);
                         }
                         Type::TIME => {
                             single_process!(row, output, idx, NaiveTime);
+                        }
+                        Type::TIMESTAMP => {
+                            single_process!(row, output, idx, NaiveDateTime);
+                        }
+                        Type::BOOL => {
+                            single_process!(row, output, idx, bool, bool_to_str);
                         }
                         Type::INT2_ARRAY => {
                             array_process!(row, output, idx, i16);
@@ -209,14 +268,64 @@ impl sqllogictest::AsyncDB for PostgresExtended {
                         Type::INT8_ARRAY => {
                             array_process!(row, output, idx, i64);
                         }
+                        Type::BOOL_ARRAY => {
+                            array_process!(row, output, idx, bool, bool_to_str);
+                        }
                         Type::FLOAT4_ARRAY => {
-                            array_process!(row, output, idx, f32);
+                            array_process!(row, output, idx, f32, float4_to_str);
                         }
                         Type::FLOAT8_ARRAY => {
-                            array_process!(row, output, idx, f64);
+                            array_process!(row, output, idx, f64, float8_to_str);
                         }
                         Type::NUMERIC_ARRAY => {
                             array_process!(row, output, idx, Decimal);
+                        }
+                        Type::DATE_ARRAY => {
+                            array_process!(row, output, idx, NaiveDate);
+                        }
+                        Type::TIME_ARRAY => {
+                            array_process!(row, output, idx, NaiveTime);
+                        }
+                        Type::TIMESTAMP_ARRAY => {
+                            array_process!(row, output, idx, NaiveDateTime);
+                        }
+                        Type::VARCHAR_ARRAY | Type::TEXT_ARRAY => {
+                            array_process!(row, output, idx, String, varchar_to_str);
+                        }
+                        Type::VARCHAR | Type::TEXT => {
+                            single_process!(row, output, idx, String, varchar_to_str);
+                        }
+                        Type::FLOAT4 => {
+                            single_process!(row, output, idx, f32, float4_to_str);
+                        }
+                        Type::FLOAT8 => {
+                            single_process!(row, output, idx, f64, float8_to_str);
+                        }
+                        Type::INTERVAL => {
+                            single_process!(self, row, output, idx, Interval, INTERVAL);
+                        }
+                        Type::TIMESTAMPTZ => {
+                            single_process!(
+                                self,
+                                row,
+                                output,
+                                idx,
+                                DateTime<chrono::Utc>,
+                                TIMESTAMPTZ
+                            );
+                        }
+                        Type::INTERVAL_ARRAY => {
+                            array_process!(self, row, output, idx, Interval, INTERVAL);
+                        }
+                        Type::TIMESTAMPTZ_ARRAY => {
+                            array_process!(
+                                self,
+                                row,
+                                output,
+                                idx,
+                                DateTime<chrono::Utc>,
+                                TIMESTAMPTZ
+                            );
                         }
                         _ => {
                             todo!("Don't support {} type now.", column.type_().name())
