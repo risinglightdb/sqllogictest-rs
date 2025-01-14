2459c5e5cde5dbdadeb6ed505edea0689faa9125
Bugen Zhao <i@bugenzhao.com>
2025-01-14 14:23:01 +0800

    feat(bin): abort connections before dropping temp databases in parallel run
    Signed-off-by: Bugen Zhao <i@bugenzhao.com>

--- sqllogictest-bin/src/main.rs
+++ sqllogictest-bin/src/main.rs
@@ -20,6 +20,7 @@ use sqllogictest::{
     default_column_validator, default_normalizer, default_validator, update_record_with_output,
     AsyncDB, Injected, MakeConnection, Record, Runner,
 };
+use utils::AbortOnDropHandle;
 
 #[derive(Default, Copy, Clone, Debug, PartialEq, Eq, ValueEnum)]
 #[must_use]
@@ -314,7 +315,7 @@ async fn run_parallel(
         }
     }
 
-    let mut stream = futures::stream::iter(create_databases.into_iter())
+    let mut stream = futures::stream::iter(create_databases)
         .map(|(db_name, filename)| {
             let mut config = config.clone();
             config.db.clone_from(&db_name);
@@ -322,13 +323,13 @@ async fn run_parallel(
             let engine = engine.clone();
             let labels = labels.to_vec();
             async move {
-                let (buf, res) = tokio::spawn(async move {
+                let (buf, res) = AbortOnDropHandle(tokio::spawn(async move {
                     let mut buf = vec![];
                     let res =
                         connect_and_run_test_file(&mut buf, filename, &engine, config, &labels)
                             .await;
                     (buf, res)
-                })
+                }))
                 .await
                 .unwrap();
                 (db_name, file, res, buf)
@@ -395,6 +396,10 @@ async fn run_parallel(
         "\n All test cases finished in {} ms",
         start.elapsed().as_millis()
     );
+
+    // Abort running cases and drop the session connections to the DB server
+    // before dropping temporary databases.
+    drop(stream);
 
     for db_name in db_names {
         if keep_db_on_failure && failed_db.contains(&db_name) {
@@ -829,4 +834,31 @@ async fn update_record<M: MakeConnection>(
     }
 
     Ok(())
+}
+
+mod utils {
+    use std::{
+        future::Future,
+        pin::Pin,
+        task::{Context, Poll},
+    };
+
+    use tokio::task::{JoinError, JoinHandle};
+
+    /// A wrapper around a [`tokio::task::JoinHandle`], which aborts the task when it is dropped.
+    pub struct AbortOnDropHandle<T>(pub JoinHandle<T>);
+
+    impl<T> Drop for AbortOnDropHandle<T> {
+        fn drop(&mut self) {
+            self.0.abort();
+        }
+    }
+
+    impl<T> Future for AbortOnDropHandle<T> {
+        type Output = Result<T, JoinError>;
+
+        fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+            Pin::new(&mut self.0).poll(cx)
+        }
+    }
 }
